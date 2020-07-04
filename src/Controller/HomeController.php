25506eaf2de0352000dc428666e47361c239a9ae
Lionel_Dala <dala.lionel@gmail.com>
2020-07-04 09:49:07 +0200

    AJOUT des catégories sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +13,24 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ProductRepository $repository)
+    public function index(ProductRepository $repository, CategoryRepository $repo)
     {
+        $categories=$repo->findAll();
         $result = $repository->findNewProducts();
-        return $this->render('home/index.html.twig', ['new_products'=>$result]);
+        return $this->render('home/index.html.twig', [
+            'new_products'=>$result,
+            'categories'=>$categories]);
+    }
+
+    /**
+     * @Route("/{id}/CategoryProducts",name="CategoryProducts")
+     */
+    public function showCategories(Category $category,$id)
+    {
+        $products= $category->getProducts();
+        return $this->render('home/productsfromcategory.html.twig',[
+            'category'=>$category,
+            'produitscategorie'=>$products
+        ]);
     }
 }
